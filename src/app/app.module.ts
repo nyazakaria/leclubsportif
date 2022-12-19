5c1c6594bb5be2c04b84b7d1156dee35b2f5b5ff
Zakaria NYA <nyzakaria@icloud.com>
2022-12-19 10:02:21 +0100

    add layout to leclubsportif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidemenuComponent } from './layout/sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
