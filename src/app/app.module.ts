807e8fe18f23d010aebfd513f1443276e5a3475b
Zakaria NYA <nyzakaria@icloud.com>
2022-12-19 12:24:27 +0100

    add form new account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,13 +37,10 @@ import { PresentationComponent } from './pages/presentation/presentation.compone
     ContactsComponent,
     NewclubComponent,
     RecrutementComponent,
-    PresentationComponent
+    PresentationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/newaccount/newaccount.component.ts
+++ src/app/pages/newaccount/newaccount.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-newaccount',
   templateUrl: './newaccount.component.html',
-  styleUrls: ['./newaccount.component.scss']
+  styleUrls: ['./newaccount.component.scss'],
 })
 export class NewaccountComponent implements OnInit {
-
-  constructor() { }
+  createAccountForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.newAccountForm();
   }
 
+  newAccountForm() {
+    this.createAccountForm = this.fb.group({
+      email: [
+        '',
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+      ],
+      passWord: [
+        '',
+        Validators.required,
+        Validators.minLength(7),
+        Validators.maxLength(15),
+      ],
+      passwordConfirm: [
+        '',
+        Validators.required,
+        Validators.pattern(this.createAccountForm.controls['passWord'].value),
+      ],
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      anneeNaissance: ['', Validators.required],
+      adresse: ['', Validators.required],
+      adressecmpl: ['', Validators.required],
+      ville: ['', Validators.required],
+      codePostal: ['', Validators.required],
+    });
+  }
+
+  createNewAccount() {}
 }
